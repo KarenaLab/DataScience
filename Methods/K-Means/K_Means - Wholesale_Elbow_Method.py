0163de37ee4697090757e4ef23f2587268c2c12c
Eduardo Chikui <ekchikui@gmail.com>
2021-10-05 00:03:44 -0300

    Update K_Means - Wholesale_Elbow_Method.py

--- Methods/K-Means/K_Means - Wholesale_Elbow_Method.py
+++ Methods/K-Means/K_Means - Wholesale_Elbow_Method.py
@@ -43,23 +43,55 @@ for col in Categorical_Features:
     DF.drop(col, axis= 1, inplace= True)
 
 
-
 MMS = MinMaxScaler()
 MMS.fit(DF)
 DF_Transformed = MMS.transform(DF)
 
-
 No_K = []
-Sum_of_Squared_Distances = []
+Sum_Dist2 = []
 
+Min_K = 2
+Max_K = 15
  
-for k in range(1,15):
+for k in range(Min_K, (Max_K+1)):
     
     KM = KMeans(n_clusters= k)
     KM = KM.fit(DF_Transformed)
 
     No_K.append(k)
-    Sum_of_Squared_Distances.append(KM.inertia_)
+    Sum_Dist2.append(KM.inertia_)
+
+
+x1, x2 = No_K[0], No_K[-1]
+y1, y2 = Sum_Dist2[0], Sum_Dist2[-1]
+
+Distance = []
+Dist_Max = 0
+Best_K = 0
+
+for i in range(0, len(No_K)):
+
+    x = No_K[i]
+    y = Sum_Dist2[i]
+
+    # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+
+    #                        |(x2-x1)*(y1-y) - (y2-y1)*(x1-x)|      Num
+    #  D(P1, P2, (x, y)) = ------------------------------------- = ----- 
+    #                           sqrt((x2-x1)^2 + (y2-y1)^2)         Den
+
+
+    Numerator = np.absolute(((x2-x1)*(y1-y)) - ((y2-y1)*(x1-x)))
+    Denominator = np.sqrt((x2-x1)**2 + (y2-y1)**2)
+    Dist = np.round(Numerator/Denominator, decimals= 6)
+    
+    Distance.append(Dist)
+
+    if(Dist > Dist_Max):
+
+        Dist_Max = Dist
+        Best_K = x
+        Best_Y = y
 
 
 # Plotting
@@ -70,13 +102,16 @@ fig = plt.figure()
 Title = "K-Means Elbow Method - Wholesales Database"
 plt.suptitle(Title, fontsize= 14)
 
-plt.plot(No_K, Sum_of_Squared_Distances,
-         color= "navy", linestyle= "-", marker= "o")
+plt.plot(No_K, Sum_Dist2,
+         color= "navy", linestyle= "-", marker= "o", zorder= 11)
+
+plt.scatter(Best_K, Best_Y, color= "darkred", marker= "o", zorder= 12)
+plt.axvline(x= Best_K, color= "black", linestyle= "--", linewidth= 0.5, zorder= 10)
 
 plt.xlabel("k")
 plt.ylabel("Sum of Squared Distances")
 
-#plt.savefig(Title, dpi= 240)
+plt.savefig(Title, dpi= 240)
 plt.show()
 
 
