096182e4abaabb161d01075cec4a46945d9ff264
EKChikui <EKChikui@gmail.com>
2022-05-10 20:06:09 -0300

    Update ColumnsClassifier.py
    Columns Classifier

--- Data Wrangling/Columns Classifier/ColumnsClassifier.py
+++ Data Wrangling/Columns Classifier/ColumnsClassifier.py
@@ -1,6 +1,16 @@
 # Database Classificator
 
-def ColumnsClassifier(DF):
+def ColumnsClassifier(DF, cat_size=0.25, **kwargs):
+    """
+    Classifier for columns as Numeric, Categoric or Text.
+
+    Variables:
+    * cat_size = if: x >= 1     Number of items to be a threshold,
+                     0 < x < 1  Percentage of items relative to the
+                                size of DataFrame.
+                                default = 0.25
+    
+    """
 
     import numpy as np
     import pandas as pd
@@ -12,49 +22,44 @@ def ColumnsClassifier(DF):
     # Versions ---------------------------------------------------------
 
     # 01 - Oct 14th, 2021 - Starter
-    # 02 - 
-
-
-    # List of Variables and kwargs -------------------------------------
-
-    #
-    #
+    # 02 - Apr 25th, 2022 - Adjusting variables and adding cat_size
+    #                       variable.
+    # 03 - 
 
 
     # Program ----------------------------------------------------------
 
-    DF_Columns = DF.columns
+    DF_columns = DF.columns
 
-    Numeric = []
-    Categoric = []
-    Text = []
+    numeric = []
+    categoric = []
+    text = []
 
-    Cat_Size = 50
+    if(cat_size < 1):
+        cat_size = DF.shape[0] * cat_size
 
+    cat_size = int(cat_size + 0.5)
 
-    for col in DF_Columns:
-
-        Data = DF[col]
-        Data_Unique = Data.nunique()
-
-
-        if(is_numeric_dtype(Data) == True):
-
-            if(Data_Unique > Cat_Size):
-                Numeric.append(col)
-
-            else:
-                Categoric.append(col)
+    for col in DF_columns:
+        data = DF[col]
+        data_unique = data.nunique()
 
 
-        if(is_string_dtype(Data) == True):
-
-            if(Data_Unique <= Cat_Size):
-                Categoric.append(col)
+        if(is_numeric_dtype(data) == True):
+            if(data_unique > cat_size):
+                numeric.append(col)
 
             else:
-                Text.append(col)
+                categoric.append(col)
 
 
-    return Numeric, Categoric, Text
+        if(is_string_dtype(data) == True):
+            if(data_unique <= cat_size):
+                categoric.append(col)
+
+            else:
+                text.append(col)
+
+
+    return numeric, categoric, text
 
