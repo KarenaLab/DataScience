52023d5d3532c703d6cec08bcd2d75517c34fa38
EKChikui <EKChikui@gmail.com>
2022-06-28 21:46:32 -0300

    Update EDA.py
    Adding EDA Gaussian (not complete, yet)

--- Data Wrangling/EDA/EDA.py
+++ Data Wrangling/EDA/EDA.py
@@ -2,9 +2,14 @@
 # EDA (Exploratory Data Analysis) for first look.
 
 # Versions -------------------------------------------------------------
-#01 - Jun 04th, 2022 - Starter
+# 01 - Jun 04th, 2022 - Starter
+# 02 - Jun 13th, 2022 - Adjusting and optmizing functions for Categoric
+# 03 - Jun 27th, 2022 - Adding EDA Gaussian
+# 04 -
+
 
 # Insights
+# Create a Date and/or Time EDA
 #
 
 
@@ -17,9 +22,11 @@ from pandas.api.types import is_numeric_dtype, is_string_dtype
 
 def EDA_simple(DF, catnum=10, decimals=5, verbose=True):
     """
-    Returns a simple EDA (Exploratory Data Analysis) for dataframe.
+    Returns a simple EDA (Exploratory Data Analysis) for dataframe
+    or Series.
     
     """
+
     data = DF.copy()
 
     if(isinstance(data, pd.Series) == True):
@@ -30,9 +37,27 @@ def EDA_simple(DF, catnum=10, decimals=5, verbose=True):
             simple_(data[col], catnum, decimals, verbose)
 
 
+def EDA_gaussian(DF, catnum=10, decimals=5, verbose=True):
+    """
+    Returns a Gaussian EDA (Exploratory Data Analysis) for dataframe
+    or Series.
+    
+    """
+
+    data = DF.copy()
+
+    if(isinstance(data, pd.Series) == True):
+        gaussian_(data, catnum, decimals, verbose)
+
+    if(isinstance(data, pd.DataFrame) == True):
+        for col in data.columns:
+            gaussian_(data[col], catnum, decimals, verbose)
+    
+
+
 def simple_(data, catnum, decimals, verbose):
     """
-    Internal function for EDA.
+    Internal function for EDA (Simple).
     
     """
 
@@ -62,6 +87,7 @@ def simple_(data, catnum, decimals, verbose):
         # Type = Categoric
         if(data_type == "Categoric"):
             data_values = data.unique()
+            data_values = np.sort(data_values)
 
 
         # Printing
@@ -93,4 +119,82 @@ def simple_(data, catnum, decimals, verbose):
             print(f' **** col "{data_name}" IS NOT Numeric ****')
             # Improve texts controls
 
+
+def gaussian_(data, catnum, decimals, verbose):
+    """
+    Internal function for EDA (Gaussian).
+
+    """
+
+    print("")
+    data_name = data.name
+    data_nunique = data.nunique()
+
+    if(is_numeric_dtype(data) == True):
+        if(data_nunique > catnum):
+            data_type = "Numeric"
+            
+            data_count = data.count()
+            data_nan = data.isna().sum()
+
+            data_min = data.min()
+            data_max = data.max()           
+            data_mean = data.mean()
+            data_stddev = data.std()
+            data_q25 = data.quantile(q=.25)
+            data_median = data.quantile(q=.50)
+            data_q75 = data.quantile(q=.75)
+
+            data_iqr = data_q75 - data_q25
+            data_range = data_max - data_min
+            data_rangecoef = (data_max - data_min)/(data_max + data_min)
+            data_quartiledeviation = (data_q75 - data_q25)/(data_q75 + data_q25)
+
+            data_lowerlimit = data_mean - (3*data_stddev)
+            data_upperlimit = data_mean + (3*data_stddev)
+            data_outlierslow = (data < data_upperlimit).sum()
+            data_outliersup = (data > data_upperlimit).sum()
+            data_outliers = data_outlierslow + data_outliersup
+
+            data_meandeviation = (data_mean - data_median)/data_stddev
+
+
+            # Printing
+            print(f" *** col: {data_name} ***")
+            print(f"    Type: {data_type}") 
+            print(f" Uniques: {data_nunique}")
+            print(f"   Count: {data_count}")
+            print(f"    NaNs: {data_nan}")
+            print("")
+            print(f"    Mean: {np.round(data_mean, decimals=decimals)}")
+            print(f"  StdDev: {np.round(data_stddev, decimals=decimals)}")
+            print(f"     Min: {np.round(data_min, decimals=decimals)}")
+            print(f"    q25%: {np.round(data_q25, decimals=decimals)}")
+            print(f"  Median: {np.round(data_median, decimals=decimals)}")
+            print(f"    q75%: {np.round(data_q75, decimals=decimals)}")
+            print(f"     Max: {np.round(data_max, decimals=decimals)}")
+            print("")
+            print(f"         IQR: {np.round(data_iqr, decimals=decimals)}")
+            print(f"       Range: {np.round(data_range, decimals=decimals)}")
+            print(f"  Range Coef: {np.round(data_rangecoef, decimals=decimals)}")
+            print(f"Quartile Dev: {np.round(data_quartiledeviation, decimals=decimals)}")
+            print(f"   Mean Dev*: {np.round(data_meandeviation, decimals=decimals)}")
+            print("")
+            print(f"    Outliers: {data_outliers}")
+            print(f"Outliers Low: {data_outlierslow}")
+            print(f" Outliers Up: {data_outliersup}")
+
+            # print function to copy and paste
+            # print(f": {np.round(data_, decimals=decimals)}")
+
+            
+        else:
+            print(f' **** col "{data_name}" IS Categoric ****')
+
+
+    else:
+        if(verbose == True):
+            print(f' **** col "{data_name}" IS NOT Numeric ****')
+            
+    
             
