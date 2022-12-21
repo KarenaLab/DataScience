3772f5133d2e6b2f7512d144d8e306c4f79c20b7
EKChikui <EKChikui@gmail.com>
2022-12-20 22:44:53 -0300

    Update prime-numbers-finding.py

--- Tools/Prime Numbers/prime-numbers-finding.py
+++ Tools/Prime Numbers/prime-numbers-finding.py
@@ -6,9 +6,9 @@ from prime_numbers_v01 import *
 
 # Program ---------------------------------------------------------------
 
-limit = [10, 100, 1000, 10000]
+limit_list = [10, 100, 500, 1000, 2000, 5000, 10000, 20000]
 
-for i in limit:
-    prime_number_list, _ = prime_numbers(i)
-    print(f" > from 1 to {i} has {len(prime_number_list)} prime numbers.")
+for limit in limit_list:
+    prime_number_list, time = prime_numbers(limit)
+    print(f" > from 1 to {limit} has {len(prime_number_list)} prime numbers. [{time:.4f} s]")
     
