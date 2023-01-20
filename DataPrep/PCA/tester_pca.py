1c4026769cc5f46e2af7d38e2f67daa433c4fded
EKChikui <EKChikui@gmail.com>
2023-01-20 17:37:43 -0300

    Create tester_pca.py

---
+++ DataPrep/PCA/tester_pca.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import pandas as pd
+
+from PCA_analysis_v01 import *
+
+
+df = pd.read_csv("winequality_red.csv", sep=",", encoding="utf-8")
+
+target = "quality"
+df_var = df.drop(columns=[target])
+df_target = df[target]
+
+title = "PCA Analysis - Wine Quality Red dataset"
+df_pca, pca_variance = pca_analysis(df_var, title)
+
