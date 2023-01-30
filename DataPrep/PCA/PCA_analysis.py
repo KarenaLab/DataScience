517638fac0cbc624b17569a2239e362cec7b7716
EKChikui <EKChikui@gmail.com>
2023-01-30 20:29:22 -0300

    Update PCA_analysis.py

--- DataPrep/PCA/PCA_analysis.py
+++ DataPrep/PCA/PCA_analysis.py
@@ -1,9 +1,19 @@
 
-def pca_analysis(DataFrame, title, **kwargs):
+def pca_analysis(DataFrame, title=None, decimals=4, color="darkblue",
+                 plot=True, savefig=False, verbose=True):
     """
     Applies the PCA (Principal Component Analysis) into the **DataFrame**
     and returns the [0] Dataframe with PCA and [1] the explained variance
     ratio accumulated.
+
+    Variables:
+    * DataFrame = Dataframe to be analised,
+    * title = Title for the plot and (if savefig=True), the name for the file,
+    * decimals = Number of decimals to be plot (default=4)
+    * color = Line color for the plot (default="darkblue")
+    * plot = True* or False, plots the accumulated PCA explanation
+    * savefig = True or False*, to save the plot as a figure
+    * verbose = True* or False, to print important information
 
     """
     # Libraries
@@ -15,6 +25,10 @@ def pca_analysis(DataFrame, title, **kwargs):
 
     import matplotlib.pyplot as plt
     import matplotlib.gridspec as gridspec
+
+    # Data preparation
+    if(title == None):
+        title = "PCA Analysis"
 
 
     # PCA
@@ -29,23 +43,30 @@ def pca_analysis(DataFrame, title, **kwargs):
     
     pct_variance = pca.explained_variance_ratio_
     pct_variance = np.cumsum(pct_variance)
-    pct_var_plot = np.round(pct_variance*100, decimals=4)
+    pct_var_plot = np.round(pct_variance*100, decimals=decimals)
 
 
     # Plotting
-    fig = plt.figure(figsize=[8, 4.5])
-    fig.suptitle(title, fontsize=10, fontweight="bold")
+    if(plot == True):
+        fig = plt.figure(figsize=[8, 4.5])
+        fig.suptitle(title, fontsize=10, fontweight="bold")
 
-    plt.plot(range(1, len(pct_var_plot)+1), pct_var_plot, color="darkblue", linewidth=1.5, zorder=20)
-    plt.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
-    plt.ylabel("% Accumulated", loc="top")
-    plt.xlabel("PCA Components", loc="right")
+        plt.plot(range(1, len(pct_var_plot)+1), pct_var_plot, color=color, linewidth=1.5, zorder=20)
+        plt.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5, zorder=1)
+        plt.ylabel("% Accumulated", loc="top")
+        plt.xlabel("PCA Components", loc="right")
+
+        plt.tight_layout()
+        if(savefig == True):
+            plt.savefig(title, dpi=240)
+            if(verbose == True):
+                print(f" > saving file {title}.png")
+
+        else:
+            plt.show()
+
+        plt.close(fig)
     
-    plt.show()
-
-
-    # Return information
 
     return df_pca, pct_variance
 
-    
