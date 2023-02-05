cfcf19819c6777cf46c6e63138c054af1b343c14
EKChikui <EKChikui@gmail.com>
2023-02-05 09:25:32 -0300

    Update tester_binning.py
    plot comparing bin sizes through methods

--- Tools/binning/tester_binning.py
+++ Tools/binning/tester_binning.py
@@ -1,15 +1,40 @@
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 from binning_v02 import *
 
+sqrt_list, rice_list, sturges_list = [], [], []
+size = [10, 25, 50, 75, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000]
 
-data_size = [10, 50, 100, 102, 400, 500, 950, 1000, 1024, 1200, 1300,
-             1400, 2000, 2222, 2345]
+for i in size:
+    bins_sqrt = binning(i, method="sqrt")
+    bins_rice = binning(i, method="rice")
+    bins_sturges = binning(i, method="sturges")
 
-for i in data_size:
-    #method: sqrt*, rice, sturges and freedman methods. 
-    #bins = binning(i, method="sqrt")
-    bins = binning(i, IQR=50, method="Freedman-Diaconis")
-    print(f" > Size: {i} -> Bins: {bins}")
+    sqrt_list.append(bins_sqrt)
+    rice_list.append(bins_rice)
+    sturges_list.append(bins_sturges)
+
+colors = ["navy", "darkred", "orange"]
+names = ["Square root", "Rice rule", "Sturges"]
+methods = [sqrt_list, rice_list, sturges_list]
+
+
+# Plot
+fig = plt.figure(figsize=[8, 4.5])
+fig.suptitle("Binning methods comparison", fontsize=10, fontweight="bold")
+
+for data, label, color in list(zip(methods, names, colors)):
+    plt.plot(size, data, color=color, label=label, zorder=20)
+
+plt.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
+plt.ylabel("Number of bins calculated", fontsize=10, loc="top")
+plt.xlabel("Number of items", fontsize=10, loc="right")
+
+plt.legend(loc="upper left", fontsize=8, framealpha=1)
+
+plt.tight_layout()
+plt.show()
 
