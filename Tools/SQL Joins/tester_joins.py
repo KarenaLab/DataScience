9c4db4ba05a6a3621c6354e92971d0ef14a25e11
EKChikui <EKChikui@gmail.com>
2023-02-13 20:43:14 -0300

    Update tester_joins.py

--- Tools/SQL Joins/tester_joins.py
+++ Tools/SQL Joins/tester_joins.py
@@ -2,17 +2,22 @@
 from sql_joins_v01 import *
 
 
-left = [1, 2, 3, 4, 5, 6]
-right = [4, 5, 6, 7, 8]
+left = [1, 2, 3, 4, 5]
+right = [4, 5, 6, 7]
 
-print(f" >  left list = {left}")
-print(f" > right list = {right} \n")
-
-i_join = inner_join(left, right)
-print(f" >  inner join: {i_join}")
+print(f" >   left list = {left}")
+print(f" >  right list = {right} \n")
 
 l_join = left_join(left, right)
-print(f" >   left join: {l_join}")
+print(f" >    left join: {l_join}")
+
+i_join = inner_join(left, right)
+print(f" >   inner join: {i_join}")
+
+r_join = left_join(right, left)
+print(f' > "right_join": {r_join}')
 
 o_join = outter_join(left, right)
-print(f" > outter join: {o_join} \n")
+print(f" >  outter join: {o_join}\n")
+
+
