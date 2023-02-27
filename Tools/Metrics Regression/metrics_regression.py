efc8e08a398e62098fc3d45e062479cfe847a812
EKChikui <EKChikui@gmail.com>
2023-02-27 20:08:42 -0300

    Update metrics_regression.py

--- Tools/Metrics Regression/metrics_regression.py
+++ Tools/Metrics Regression/metrics_regression.py
@@ -6,16 +6,25 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
 
-def regr_metrics(y_test, y_pred, metrics="all", verbose=True):
+def regr_metrics(y_true, y_pred, metrics="all", verbose=True):
     """
-    Calculates all regression metrics.
-    
+    Calculates all regression metrics and returns a dictionary with all
+    values selected.
 
+    Variables:
+    * y_true = Variable to be compared,
+    * y_pred = Variable predicted and to be compared to,
+
+    * MAE = Mean Absolute Error
+    * MSE = Mean Squared Error
+    * R2 = Coef. of Determination
+    * Pearson = Pearson correaltion coef.
+    
     """
     metrics_dict = {}
     
     if(metrics.count("mae") == 1 or metrics.count("all") == 1):
-        mae = mean_absolute_error(y_test, y_pred)
+        mae = mean_absolute_error(y_true, y_pred)
         metrics_dict["mae"] = mae
 
         if(verbose == True):
@@ -23,7 +32,7 @@ def regr_metrics(y_test, y_pred, metrics="all", verbose=True):
 
 
     if(metrics.count("mse") == 1 or metrics.count("all") == 1):
-        mse = mean_squared_error(y_test, y_pred)
+        mse = mean_squared_error(y_true, y_pred)
         metrics_dict["mse"] = mse
 
         if(verbose == True):
@@ -31,7 +40,7 @@ def regr_metrics(y_test, y_pred, metrics="all", verbose=True):
 
             
     if(metrics.count("r2") == 1 or metrics.count("all") == 1):        
-        r2 = r2_score(y_test, y_pred)
+        r2 = r2_score(y_true, y_pred)
         metrics_dict["r2_score"] = r2
 
         if(verbose == True):
@@ -39,7 +48,7 @@ def regr_metrics(y_test, y_pred, metrics="all", verbose=True):
 
 
     if(metrics.count("pearson") == 1 or metrics.count("all") == 1):
-        pearson = np.corrcoef(y_test, y_pred)[0][1]
+        pearson = np.corrcoef(y_true, y_pred)[0][1]
         metrics_dict["pearson"] = pearson
         
         if(verbose == True):
