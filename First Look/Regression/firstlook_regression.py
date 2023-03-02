2abb9baf4cfabb8313f9ee217045370e0820baaf
EKChikui <EKChikui@gmail.com>
2023-03-01 22:48:25 -0300

    Update firstlook_regression.py

--- First Look/Regression/firstlook_regression.py
+++ First Look/Regression/firstlook_regression.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
 
 
 # Personal modules
@@ -93,7 +94,6 @@ def regr_lr_lasso(x_train, y_train, x_test, y_test,
     
     regr_params = regr.get_params()
     regr_params.pop("copy_X")
-
     regr_params = {"regr_name": "LinRegr Lasso"} | regr_params
 
     # Fit, predict and coeficients
@@ -104,4 +104,33 @@ def regr_lr_lasso(x_train, y_train, x_test, y_test,
     results = regr_metrics(y_test, y_pred, metrics=metrics, verbose=verbose)
 
     return weights, regr_params, results
-                  
+
+
+def regr_elasticnet(x_train, y_train, x_test, y_test,
+                    alpha=1, l1_ratio=0.5, fit_intercept=True, positive=False,
+                    metrics="all", verbose=True):
+    """
+    Performs a Linear Regression combined L1 and L2 priors as regularizer.
+    using scikit-learn library.
+
+    """
+    # Model = Linear Regression with ElasticNet (L1 and L2) penalisation
+    regr = ElasticNet()
+
+    # Model parameters
+    regr.set_params(alpha=alpha)
+    regr.set_params(l1_ratio=l1_ratio)
+
+    regr_params = regr.get_params()
+    regr_params.pop("copy_X")
+    regr_params = {"regr_name": "LinRegr ElasticNet"} | regr_params
+
+    # Fit, predict and coeficients
+    regr.fit(x_train, y_train)
+    y_pred = regr.predict(x_test)
+
+    weights = {"intercept": regr.intercept_, "coefs": regr.coef_}
+    results = regr_metrics(y_test, y_pred, metrics=metrics, verbose=verbose)
+
+    return weights, regr_params, results
+
