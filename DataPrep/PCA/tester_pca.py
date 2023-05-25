543b4dbd26540bf5ec84d8d2e6c997bff1a7824e
Eduardo Chikui <EKChikui@gmail.com>
2023-05-25 16:46:05 -0300

    Update tester_pca.py

--- DataPrep/PCA/tester_pca.py
+++ DataPrep/PCA/tester_pca.py
@@ -1,16 +1,35 @@
 
+# Libraries
 import numpy as np
 import pandas as pd
 
-from PCA_analysis_v01 import *
+from pca_analysis_v02 import *
 
 
+# Functions
+def split_target(DataFrame, target):
+    """
+    Splits **Dataframe** into x (variables) and y (target).
+
+    """
+    if(DataFrame.columns.tolist().count(target) == 1):
+        x = df.drop(columns=[target])
+        y = df[target]
+
+    else:
+        x = np.nan
+        y = np.nan
+
+
+    return x, y
+        
+
+
+# Program --------------------------------------------------------------
 df = pd.read_csv("winequality_red.csv", sep=",", encoding="utf-8")
 
-target = "quality"
-df_var = df.drop(columns=[target])
-df_target = df[target]
+x, y = split_target(df, target="quality")
 
 title = "PCA Analysis - Wine Quality Red dataset"
-df_pca, pca_variance = pca_analysis(df_var, title, savefig=False)
+df_pca, pca_variance = pca_analysis(x, title, savefig=False)
 
