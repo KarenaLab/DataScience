b5900c9c6c9af4cd2174f34c8e5af799e04b1d6e
Eduardo Chikui <EKChikui@gmail.com>
2023-06-20 23:32:13 -0300

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,93 +1,98 @@
-# EDA
+# EDA ------------------------------------------------------------------
 
-def EDA(data, decimals=5, **kwargs):
+# Versions
+# 01 - Apr 22nd, 2022 - Launch
+# 02 - Jun 20th, 2023 - Standards and using print_metrics
+
+
+# Insights
+# Choose what type of information to return
+
+
+# Libraries
+import numpy as np
+
+from scipy.stats import kurtosis
+from scipy.stats import shapiro
+
+
+
+def EDA(data, decimals=5, verbose=True):
     """
     Performs EDA (Exploratory Data Analysis).
-    Information returned:
-    Numeric:   No. Items, No. NaNs, Mean, Standard Deviation, Minimum,
-               Maximum, Range (or Amplitude) 25% Quantile, Median and
-               75% Quantile.
 
-    Categoric: No. Unique Items, Value Counting.
-    
+
     """
+    # Data preparation
+    data = np.array(data)
 
-    # Libraries
-    import numpy as np
-    import pandas as pd
+    data_count = data.size
+    data_nan = np.isnan(data).sum()
+    data_valid = data_count - data_nan
 
-    from pandas.api.types import is_numeric_dtype
-    from pandas.api.types import is_string_dtype
-    from pandas.api.types import is_bool_dtype
+    # Remove nans to perform all others metrics
+    data = data[~np.isnan(data)]
 
-
-    # Versions ---------------------------------------------------------
-    # 01 - Apr 22nd, 2022 - Launch
-    # 02 -
-
-
-    # Insights
-    # Choose what type of information to return
+    # Simple EDA 
+    data_unique = np.unique(data)
+    data_nunique = data_unique.size
     
+    data_mean = np.mean(data)
+    data_stddev = np.std(data)
+
+    data_min = np.min(data)
+    data_max = np.max(data)
+    data_range = data_max - data_min
+
+    data_q25 = np.quantile(data, q=0.25)
+    data_median = np.quantile(data, q=0.50)
+    data_q75 = np.quantile(data, q=0.75)
+    data_iqr = data_q75 - data_q25
 
 
-    # Program ----------------------------------------------------------
+    # Advanced EDA
+    data_kurtosis = kurtosis(data)
+    wk_stat, wk_pvalue = shapiro(data)
 
-    # Data = Pandas Series
-    if(isinstance(data, list) == True):
-        data = pd.Series(data)
+    if(wk_pvalue < 0.05):
+        status = "parametric"
 
-    if(isinstance(data, np.ndarray) == True):
-        data = pd.Series(data)
-
-   
-    # EDA
-    data_label = data.name
-    data_size = data.size
+    else:
+        status = "non_parametric"
 
 
-    # Testing Types
-    is_numeric = is_numeric_dtype(data)
-    is_text = is_string_dtype(data)
-    is_bool = is_bool_dtype(data)
+    #if(wk_status == "parametric"):
+        # Standard Wasserstein Distance
+
+    # Bins Numbers
+    bins_min, bins_min_param = float("inf"), ""
+    bins_max, bins_max_param = 0, ""
+
+    bins_strategy = ["fd", "doane", "scott", "stone", "rice", "sturges", "sqrt"]
+    for strategy in bins_strategy:
+        bins = np.histogram_bin_edges(data, bins=strategy).size
+
+        if(bins > bins_max):
+            bins_max = bins
+            bins_max_param = strategy
+
+        if(bins < bins_min):
+            bins_min = bins
+            bins_min_param = strategy
+
+
+    metrics_dict = {"count": data_count, "nan": data_nan, "valid": data_valid, "uniques": data_unique,
+                    "mean": data_mean, "stddev": data_stddev,
+                    "minimum": data_min, "maximum": data_max, "range": data_range,
+                    "q 25%": data_q25, "median": data_median, "q 75%": data_q75, "IQR": data_iqr,
+                    "kurtosis": data_kurtosis, "wk_pvalue": wk_pvalue, "status": status,
+                    "bins minimum": bins_min, "bins maximum": bins_max}
+
+
+    if(verbose == True):
+        pass
     
-
-    # EDA for Numeric
-    if(is_numeric == True):
-        data_nan = np.isnan(data).sum()
-        data_min = data.min()
-        data_max = data.max()
-        data_range = data_max - data_min
-
-        data_mean = data.mean()
-        data_stddev = data.std()
-
-        data_q25 = np.quantile(data, q=0.25)
-        data_median = np.quantile(data, q=0.50)
-        data_q75 = np.quantile(data, q=0.75)
-
-        # Showing information
-        print(f" >>> Column: {data_label} \n")
-        print(f"  No. Items: {data_size}")
-        print(f"   No. NaNs: {data_nan}")
-        print(f"       Mean: {np.round(data_mean, decimals=decimals)}")
-        print(f"     StdDev: {np.round(data_stddev, decimals=decimals)}")
-        print(f"      Range: {np.round(data_range, decimals=decimals)}")
-        print(f"    Minimum: {np.round(data_min, decimals=decimals)}")
-        print(f"       q_25: {np.round(data_q25, decimals=decimals)}")
-        print(f"     Median: {np.round(data_median, decimals=decimals)}")
-        print(f"       q_75: {np.round(data_q75, decimals=decimals)}")
-        print(f"    Maximum: {np.round(data_max, decimals=decimals)}")
-        print("")
-
-
-    # EDA for Categoric
-    if(is_text == True):
-
-        # Showing information
-        print(f" >>> Column: {data_label}")
-        print(f" >>> data is CATEGORIC")
-        print("")
     
-        
-    
+    return metrics_dict
+
+
