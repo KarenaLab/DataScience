f8818e6bee38e77dcd5ee184024f0e493f3d5804
Seb Karena <EKChikui@gmail.com>
2023-07-04 20:21:50 -0300

    Create string_to_date.py

---
+++ 09_tools/datetime_management/string_to_date.py
@@ -0,0 +1,46 @@
+# String to date
+# Convert a pandas column from string to python datetime.
+
+# Versions
+# 01 - Mai 30th, 2023 - Starter
+# 02 - Jul 04th, 2023 - Adjust style and organization
+# 03 -
+
+
+# Insights, improvements and bugfix
+#    Add control of old column (to keep it or delete it)
+#    Add verbose controls and messages
+
+
+
+# Libraries
+import pandas as pd
+
+
+# ----------------------------------------------------------------------
+
+def string_to_date(DataFrame, source, destiny=""):
+    """
+    Tranforms the column source stored as string into a new column
+    destiny as python datetime format.
+
+    DataFrame = DataFrame to be transformed,
+    source = column with the datetime stored as string
+    destiny = (optional). if not informed, source will be replaced.
+
+    """   
+    data = DataFrame.copy()
+
+    if(destiny == ""):
+        destiny = source
+
+    data[destiny] = pd.to_datetime(data[source])
+
+    if(source != destiny):
+        data = data.drop(columns=[source])
+
+    data = data.sort_values(by=destiny).reset_index(drop=True)
+
+
+    return data
+
