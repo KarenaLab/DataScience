c8ee91b85428688c765095e243ceaa5e102425a5
Seb Karena <EKChikui@gmail.com>
2023-07-28 14:00:58 -0300

    Update preparation_tools.py

--- 03_preparation/tools/preparation_tools.py
+++ 03_preparation/tools/preparation_tools.py
@@ -31,3 +31,47 @@ def truncate(x, high, low=0):
     return x
 
 
+def linspacebystep(stop, step, start=0, cap=True):
+    """
+    Creates a linear space using the step as parameter for the number of values.
+
+    """
+    num = (stop - start) / step
+
+    if(cap == True):
+        num = int(num) + 1
+    
+    else:
+        num = int(num + 0.5) + 1
+    
+    space = np.linspace(start=start, stop=stop, num=num)
+
+
+    return space
+
+
+def alpha_space():
+    """
+    Creates a numpy array with common alphas to be used for Ridge and Lasso regression
+    hyperparameters.
+
+    """
+    alpha = np.append(np.array([]), linspacebystep(start=0.01, stop=0.05, step=0.01))
+    alpha = np.append(alpha, linspacebystep(start=0.05, stop=0.5, step=0.05))
+    alpha = np.append(alpha, linspacebystep(start=0.5, stop=1, step=0.1))
+    alpha = np.append(alpha, linspacebystep(start=1, stop=10, step=1))
+    alpha = np.append(alpha, linspacebystep(start=10, stop=20, step=2))
+
+    alpha = np.round(alpha, decimals=3)
+    alpha = np.sort(np.unique(alpha))
+
+
+    return alpha
+
+
+# end
+    
+
+
+
+
