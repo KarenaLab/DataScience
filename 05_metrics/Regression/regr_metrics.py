e7558eeaf8fbca274f4c8c115ffc064bbfecbc84
Seb Karena <EKChikui@gmail.com>
2023-08-29 18:59:03 -0300

    Update regr_metrics.py
    Adding Bias.

--- 05_metrics/Regression/regr_metrics.py
+++ 05_metrics/Regression/regr_metrics.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import r2_score
 
 
@@ -15,7 +16,7 @@ from sklearn.metrics import r2_score
 # Insights
 # Add SMAPE
 # Add MAPE
-#
+# Add Bias
 
 
 def regr_metrics(y_true, y_pred, metrics="all", verbose=True):
@@ -39,23 +40,52 @@ def regr_metrics(y_true, y_pred, metrics="all", verbose=True):
     if(metrics.count("pearson") == 1 or metrics == "all"):
         pearson = np.corrcoef(y_true, y_pred)[0][1]
         results["pearson"] = pearson
-
     
     if(metrics.count("mae") == 1 or metrics == "all"):
         mae = mean_absolute_error(y_true, y_pred)
         results["mae"] = mae
         
-
     if(metrics.count("rmse") == 1 or metrics == "all"):
         rmse = mean_squared_error(y_true, y_pred, squared=True)
         results["rmse"] = rmse
-        
 
-    if(metrics.count("r2") == 1 or metrics == "all"):
+    if(metrics.count("mape") == 1 or metrics == "all"):
+        mape = mean_absolute_percentage_error(y_true, y_pred)
+        results["mape"] = mape
+
+    if(metrics.count("smape") == 1 or metrics == "all"):
+        smape = regr_smape(y_true, y_pred)
+        results["smape"] = smape
+
+    if(metrics.count("bias") == 1 or metrics == "all"):
+        bias = regr_bias(y_true, y_pred)
+        results["bias"] = bias
+
+    if(metrics.count("r2_score") == 1 or metrics == "all"):
         r2 = r2_score(y_true, y_pred)
         results["r2_score"] = r2
     
 
     return results
 
+
+
+def regr_bias(y_true, y_pred):
+    """
+    Statistical **Bias**, in the mathematical field of statistics, is a systematic
+    tendency in which the methods used to gather data and generate statistics
+    present an inaccurate, skewed or biased depiction of reality.
+    Statistical bias exists in numerous stages of the data collection and
+    analysis process, including: the source of the data, the methods used to
+    collect the data, the estimator chosen, and the methods used to analyze
+    the data.
+
+    """
+    # Data preparation: Not need because it will be called after the main
+    # function. If be used in a single way, need to add np.array treatment
+
+    bias = np.mean(y_pred - y_true)
+
+
+    return bias
 
