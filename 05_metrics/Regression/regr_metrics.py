9c66ef5007c09cc2b9d4314d30c0bfc0fcbdb587
Seb Karena <EKChikui@gmail.com>
2023-09-11 00:49:08 -0300

    Update regr_metrics.py

--- 05_metrics/Regression/regr_metrics.py
+++ 05_metrics/Regression/regr_metrics.py
@@ -157,6 +157,19 @@ def append_results(DataFrame, new_results):
     return DataFrame
 
 
-# end
+def results_to_dataframe(results):
+    """
+    Converts a list of dictionaries with models results into a DataFrame.
 
+    """
+    data = pd.DataFrame(data=[])
     
+    if(isinstance(results, list) == True):
+        for i in results:
+            line = pd.Series(i).to_frame()
+            data = pd.concat([data, line.T], ignore_index=True)        
+
+
+    return data
+
+# end
