3fc177f6aae8d95cd8710df4d2c3921688467a33
Seb Karena <EKChikui@gmail.com>
2023-09-17 00:33:47 -0300

    Update regr_linearregression.py

--- 04_models/Regression/regr_linearregression.py
+++ 04_models/Regression/regr_linearregression.py
@@ -49,6 +49,7 @@ def regr_linreg(x_train, y_train, x_test, y_test,
     # HiperParameters
     model.fit_intercept = fit_intercept
     model.positive = positive
+
     hiperparams = {"model": "Linear Regression",
                    "fit_intercept": fit_intercept,
                    "positive": positive}
@@ -63,7 +64,7 @@ def regr_linreg(x_train, y_train, x_test, y_test,
     metrics = regr_metrics(y_test, y_pred, metrics=metrics)
 
     # Results
-    results = {}
+    results = dict()
     results.update(hiperparams)
     results.update(params)
     results.update(metrics)
@@ -113,7 +114,7 @@ def gridsearch_linreg(x_train, y_train, x_test, y_test, metrics="all",
 
 
 def regr_ridge(x_train, y_train, x_test, y_test,
-               alpha, fit_intercept=True, positive=False, metrics="all"):
+               alpha=1, fit_intercept=True, positive=False, metrics="all"):
     """
     Module to perform a **Ridge Regression (L2)** using scikit-learn module
     and adding some features to help to have more control over modules and
@@ -129,6 +130,7 @@ def regr_ridge(x_train, y_train, x_test, y_test,
     model.alpha = alpha
     model.fit_intercept = fit_intercept
     model.positive = positive
+
     hiperparams = {"model": "Ridge",
                    "alpha": alpha,
                    "fit_intercept": fit_intercept,
@@ -143,7 +145,7 @@ def regr_ridge(x_train, y_train, x_test, y_test,
     metrics = regr_metrics(y_test, y_pred, metrics=metrics)
 
     # Results
-    results = {}
+    results = dict()
     results.update(hiperparams)
     results.update(params)
     results.update(metrics)
@@ -151,8 +153,50 @@ def regr_ridge(x_train, y_train, x_test, y_test,
     return results, y_pred
 
 
+def gridsearch_ridge(x_train, y_train, x_test, y_test, metrics="all",
+                     alpha=[1], fit_intercept=[True, False], positive=[False, True],
+                     add_to_results=None):
+    """
+    Module to perform Grid Search with **Ridge Regression (L2)** using
+    scikit-learn module and adding some features to help to have more
+    control over modules and parameters.
+
+    Parameters are **alpha**, **fit_intercept** and **positive**.
+    For fit_intercept and positive, the standard parameter is the first
+    one. (If you wish to lock some, just declare a new list with only the
+    selected parameter).
+    
+    More info:
+    https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Ridge.html
+
+    """
+    # Hiperparameters selection for Ridge Regression (L2):
+    #  > alpha,
+    #  > fit_intercept,
+    #  > positive,
+
+
+    # Grid Search
+    gs_results = list()
+    parameters = itertools.product(alpha, fit_intercept, positive)
+    
+    for a, fi, p in parameters:
+        results, _ = regr_ridge(x_train, y_train, x_test, y_test,
+                                alpha=a, fit_intercept=fi, positive=p, metrics=metrics)
+
+        if(add_to_results != None and isinstance(add_to_results, dict) == True):
+            for index, value in zip(add_to_results.keys(), add_to_results.values()):
+                results[index] = value
+                
+
+        gs_results.append(results)
+
+      
+    return gs_results
+
+
 def regr_lasso(x_train, y_train, x_test, y_test,
-               alpha, fit_intercept=True, positive=False, metrics="all"):
+               alpha=1, fit_intercept=True, positive=False, metrics="all"):
     """
     Module to perform a **Lasso Regression (L1)** using scikit-learn module
     and adding some features to help to have more control over modules and
@@ -168,6 +212,7 @@ def regr_lasso(x_train, y_train, x_test, y_test,
     model.alpha = alpha
     model.fit_intercept = fit_intercept
     model.positive = positive
+
     hiperparams = {"model": "Lasso",
                    "alpha": alpha,
                    "fit_intercept": fit_intercept,
@@ -182,7 +227,7 @@ def regr_lasso(x_train, y_train, x_test, y_test,
     metrics = regr_metrics(y_test, y_pred, metrics=metrics)
 
     # Results
-    results = {}
+    results = dict()
     results.update(hiperparams)
     results.update(params)
     results.update(metrics)
