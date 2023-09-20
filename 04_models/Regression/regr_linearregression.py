c01b3b772f2912c9b7c3e7d6149369682e0b5ab5
Seb Karena <EKChikui@gmail.com>
2023-09-19 23:56:08 -0300

    Update regr_linearregression.py

--- 04_models/Regression/regr_linearregression.py
+++ 04_models/Regression/regr_linearregression.py
@@ -51,6 +51,7 @@ def regr_linreg(x_train, y_train, x_test, y_test,
     model.positive = positive
 
     hiperparams = {"model": "Linear Regression",
+                   "alpha": np.nan,
                    "fit_intercept": fit_intercept,
                    "positive": positive}
 
