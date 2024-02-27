3bc4c021944e7de90cf966275db9fcbd676c1a2b
Seb Karena <EKChikui@gmail.com>
2024-02-26 22:59:42 -0300

    Create one_feature_importance.py

---
+++ 07_statistics/one_feature_importance/one_feature_importance.py
@@ -0,0 +1,35 @@
+# One feature importance [P408]
+
+# Versions
+# 01 - Feb 26th, 2024 - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+from sklearn.pipeline import Pipeline
+
+
+# Setup/Config
+
+
+
+# ----------------------------------------------------------------------
+def one_feature_importance(DataFrame, target, columns="all",):
+    """
+    Calculates R2 Score for a Dataset using a Linear Regression simple
+    to check the relationship between variable and target.
+
+    """
+    #target_split
+    
+
+    return None    
+
