ae2cf7fee23cc036c4932b0dc9bb7be3d6cfe892
Seb Karena <EKChikui@gmail.com>
2024-10-13 16:55:12 -0300

    Create sql_tools.py

---
+++ 03_preparation/SQL_tools/sql_tools.py
@@ -0,0 +1,23 @@
+# Name [P440]
+# SQL Tools for Data Science
+
+# Versions
+# 01 - Oct 10th, 2024 - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import sqlite3
+import numpy as np
+import pandas as pd
+
+
+# ----------------------------------------------------------------------
+
+
+# end
+
