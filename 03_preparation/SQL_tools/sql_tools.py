2c1d30171e1b2cfa4f47a5995dc742151d18e143
Seb Karena <EKChikui@gmail.com>
2024-10-15 20:39:31 -0300

    Add `database_connection` and `database_close` functions

--- 03_preparation/SQL_tools/sql_tools.py
+++ 03_preparation/SQL_tools/sql_tools.py
@@ -3,7 +3,7 @@
 
 # Versions
 # 01 - Oct 10th, 2024 - Starter
-# 02 -
+# 02 - Oct 14th, 2024 - Add Connection and Close functions
 
 
 # Insights, improvements and bugfix
@@ -12,11 +12,28 @@
 
 # Libraries
 import sqlite3
+
 import numpy as np
 import pandas as pd
 
 
 # ----------------------------------------------------------------------
+def database_connection(filename):
+    global conn, cursor
+
+    conn = sqlite3.connect(filename)
+    cursor = conn.cursor()
+
+    return None
+
+
+def database_close():
+    global conn, cursor
+
+    cursor.close()
+    conn.close()
+
+    return None
 
 
 # end
