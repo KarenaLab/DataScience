d65a3f2a8eeea4559496f3bd3980dcb789bf552b
Seb Karena <EKChikui@gmail.com>
2024-12-22 06:54:36 -0300

    Create smoothing_techniques.py

---
+++ 08_timeseries/smoothing/smoothing_techniques.py
@@ -0,0 +1,39 @@
+# [P449] Smoothing techniques
+#
+
+# Versions
+# 01 - Dec 22rd, 2024 - Starter
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+# ----------------------------------------------------------------------
+def moving_average(Series, window):
+    """
+    Performs the moving average with a given **window**.
+
+    """
+    data = Series.rolling(window=window).mean()
+    
+
+    return data
+
+    
+# moving average
+
+# exponential smoothing
+
+# holt_winters
+
+# lowess_smoothing
+
+# kalman_filter
+
+# savitzky_golay
+
+
+# Source
+# https://pieriantraining.com/python-smoothing-data-a-comprehensive-guide/
+
