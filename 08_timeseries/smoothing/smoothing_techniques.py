c5a04f7e1fae756592d52dcb37cd5e6f2542f184
Seb Karena <EKChikui@gmail.com>
2024-12-24 11:50:38 -0300

    Update smoothing_techniques.py

--- 08_timeseries/smoothing/smoothing_techniques.py
+++ 08_timeseries/smoothing/smoothing_techniques.py
@@ -1,13 +1,24 @@
 # [P449] Smoothing techniques
-#
 
 # Versions
-# 01 - Dec 22rd, 2024 - Starter
+# 01 - Dec 22nd, 2024 - Starter
+#      Dec 23rd, 2024 - Add Exponential Moving Average (EWMA)
+#      
+
+# Insights, improvements and bugfix
+# 01 - Add Holt-Winters Exponential technique
+#
 
 
 # Libraries
 import numpy as np
 import pandas as pd
+
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.nonparametric.smoothers_lowess import lowess
+from pykalman import KalmanFilter
+from scipy.signal import savgol_filter
+
 
 # ----------------------------------------------------------------------
 def moving_average(Series, window):
@@ -17,22 +28,34 @@ def moving_average(Series, window):
     """
     data = Series.rolling(window=window).mean()
     
-
     return data
 
+
+def exponential(x, y, alpha=0.5, halflife=None):
+    """
+    Performs Exponental Weighted Moving Average (EWMA) with a given
+    **alpha**.
+
+    Important: Based on pandas.ewm function. For more parameters, use
+    that function directly.
     
-# moving average
+    More info:
+    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.ewm.html
 
-# exponential smoothing
+    """
+    data = pd.DataFrame(index=x, data=y)
+    data = data.ewm(alpha=alpha, halflife=halflife).mean()
 
-# holt_winters
+    col = data.columns[0]
+    y_ewma = data[col]
+        
+    return y_ewma
+                
+
 
 # lowess_smoothing
-
 # kalman_filter
-
 # savitzky_golay
-
 
 # Source
 # https://pieriantraining.com/python-smoothing-data-a-comprehensive-guide/
