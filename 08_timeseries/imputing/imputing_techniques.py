db1ed4056d55727a4a07888db46220ae7bc70625
Seb Karena <EKChikui@gmail.com>
2024-12-24 11:50:32 -0300

    Update imputing_techniques.py

--- 08_timeseries/imputing/imputing_techniques.py
+++ 08_timeseries/imputing/imputing_techniques.py
@@ -1,18 +1,28 @@
-# Name [Pxxx]
-# (optional) Short description
+# Time Series Imputation techniques [P452]
 
 # Versions
-# 01 - Date - Starter
+# 01 - Dec 23rd, 2024 - Starter
 # 02 -
 
 
 # Insights, improvements and bugfix
-#
+# Interpolate (linear and spline)
+# Foward fill
+# Backward fill
+# Average mean
+# Average median
+
+
+# Sources and relevant information
+# Pandas Interpolate (pandas.interpolate)
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.interpolate.html
+
+# https://www.geeksforgeeks.org/how-to-deal-with-missing-values-in-a-timeseries-in-python/
+
+# 
 
 
 # Libraries
-from collections import namedtuple
-
 import numpy as np
 import pandas as pd
 import scipy.stats as st
