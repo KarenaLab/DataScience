b596c26b4196222ab319988c9115521480b92e2e
Seb Karena <EKChikui@gmail.com>
2024-12-27 10:41:26 -0300

    Update Imputing_exercise_rainfall.py

--- 08_timeseries/imputing/Imputing_exercise_rainfall.py
+++ 08_timeseries/imputing/Imputing_exercise_rainfall.py
@@ -50,3 +50,19 @@ df_zeros = fill_zeros(df_zeros, columns=columns)
 
 df_linear = df.copy()
 df_linear = fill_interpolate(df_linear, columns=columns, method="linear")
+
+df_spline = df.copy()
+df_spline = fill_interpolate(df_spline, columns=columns, method="polynomial", order=2)
+
+df_max = df.copy()
+df_max = fill_max(df_max, columns=columns)
+
+df_min = df.copy()
+df_min = fill_min(df_min, columns=columns)
+
+df_ffill = df.copy()
+df_ffill = fill_forward(df_ffill, columns=columns)
+
+df_bfill = df.copy()
+df_bfill = fill_backward(df_bfill, columns=columns)
+
