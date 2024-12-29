3d7c432523be1740c1065e795b17daa287c9b29b
Seb Karena <EKChikui@gmail.com>
2024-12-28 21:29:53 -0300

    Create ts_sampling.py

---
+++ 08_timeseries/sampling/ts_sampling.py
@@ -0,0 +1,28 @@
+# Time Series Sampling [P453]
+
+# Versions
+# 01 - Dec 28th, 2024 - Startes
+# 02 -
+
+
+# Insights, improvements and bugfix
+# Upsampling and Downsampling
+
+
+# Libraries
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
