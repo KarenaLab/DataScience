2474b941e9668563e51ec98c334ed4a19ee07123
Seb Karena <EKChikui@gmail.com>
2025-01-15 21:53:06 -0300

    Create ts_tools.py

---
+++ 08_timeseries/tools/ts_tools.py
@@ -0,0 +1,30 @@
+# [P473] Time Series Tools
+
+# Versions
+# 01 - Jan 15th, 2025 - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
