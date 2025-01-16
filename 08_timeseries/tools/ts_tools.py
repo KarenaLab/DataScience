01ea3c7c600ff4f6b517c0e7509a356552656a7a
Seb Karena <EKChikui@gmail.com>
2025-01-15 22:22:30 -0300

    Update ts_tools.py

--- 08_timeseries/tools/ts_tools.py
+++ 08_timeseries/tools/ts_tools.py
@@ -19,12 +19,24 @@ import matplotlib.pyplot as plt
 
 
 # ----------------------------------------------------------------------
-def name():
+def mape(y_true, y_pred):
     """
-    Description of the function
-    Information about variables and **kwargs
+    Mean Absolute Percentage Error (MAPE) stands for the percentual
+    absolute error of the prediction (or forecast).
+    [MLTSF p.47]
+    
+    More info:
+    https://en.wikipedia.org/wiki/Mean_absolute_percentage_error
+    https://www.statology.org/how-to-interpret-mape/
 
     """
+    # Data preparation
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
 
-    return None    
+    # Calculation
+    mape = np.mean((np.abs(y_true - y_pred) / y_true))
+    mape = mape * 100
+    
+    return mape  
 
