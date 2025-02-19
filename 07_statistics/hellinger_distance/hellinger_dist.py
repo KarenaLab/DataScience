44cd3ebf4b0083a9952d38e189133ce88145249f
Seb Karena <EKChikui@gmail.com>
2025-02-19 18:01:34 -0300

    Hellinger distance function

--- 07_statistics/hellinger_distance/hellinger_dist.py
+++ 07_statistics/hellinger_distance/hellinger_dist.py
@@ -1,8 +1,7 @@
-# [Pxxx] Project name
-# (optional) Short description
+# [P481] Hellinger distance
 
 # Versions
-# 01 - Date - Starter
+# 01 - Feb 19th, 2025 - Starter
 # 02 -
 
 
@@ -15,17 +14,29 @@ import numpy as np
 import pandas as pd
 import scipy.stats as st
 
-import matplotlib.pyplot as plt
-
-
 
 # ----------------------------------------------------------------------
-def name():
+def hellinger_distance(sample1, sample2):
     """
-    Description of the function
-    Information about variables and **kwargs
+    Returns the Hellinger (H) distance between two probability
+    distributions.
+    
+    Parameters:
+    * sample1: True probability distribution.
+    * sample2: Approximate probability distribution.
+    
+    """   
+    # Data preparation  
+    sample1 = np.array(sample1)
+    sample2 = np.array(sample2)
+    
+    # Ensure the distributions sum to 1
+    sample1 = sample1 / np.sum(sample1)
+    sample2 = sample2 / np.sum(sample2)
+    
+    # Hellinger distance
+    H = np.sqrt(np.sum((np.sqrt(sample2) - np.sqrt(sample1)) ** 2)) / np.sqrt(2)
 
-    """
-
-    return None    
+    
+    return H   
 
