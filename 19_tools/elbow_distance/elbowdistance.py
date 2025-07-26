c304ba37fc0b1b85efa05df38c81465b68362344
Seb Karena <EKChikui@gmail.com>
2025-07-26 16:49:07 -0300

    Update elbowdistance.py

--- 19_tools/elbow_distance/elbowdistance.py
+++ 19_tools/elbow_distance/elbowdistance.py
@@ -1,11 +1,17 @@
+# [P208] Elbow Distance for K-Means clustering --------------------------
 
-def elbowdistance(pk, pi, pn):
+# Libraries
+import numpy as np
+
+
+# -----------------------------------------------------------------------
+def calc_elbowdistance(pk, pi, pn):
     """
     Calculates the distance from a line draw line between **pi** and **pn**
     to a point **pn**.
 
     where:
-    * p1 = [x1, y2] (initial)
+    * pi = [x1, y2] (initial)
     * pn = [x2, y2] (last)
     * pk = [x, y]   (k, or the variable point)
 
@@ -21,30 +27,15 @@ def elbowdistance(pk, pi, pn):
 
     """
 
-    # Versions
-    # 01 - Jun 22nd, 2022 - Starter
-    # 02 - Jan 23rd, 2023 - Adjusting variables
-    # 03 - Jan 29th, 2023 - Adding equation details and LaTex
-    # 04 - 
-
-    # Insights:
-    # Inform line as an equation, option
-    #
-
-    
-    # Program ---------------------------------------------------------
-    import numpy as np
-
     # Splitting points into coordinates (easy apply in equation)
     x1, y1 = pi[0], pi[1]
     x2, y2 = pn[0], pn[1]
     x, y = pk[0], pk[1]
 
     # Calc
-    numerator = np.abs((x2-x1)*(y1-y)) - np.abs((y2-y1)*(x1-x))
-    denominator = np.sqrt((x2-x1)**2 + (y2-y1)**2)
-    dist = numerator/denominator
-
+    numerator = np.abs((x2 - x1) * (y1 - y)) - np.abs((y2 - y1) * (x1 - x))
+    denominator = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    dist = numerator / denominator
 
     return dist
 
